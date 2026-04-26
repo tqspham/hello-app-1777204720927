b4cfd64f045929fc627758cfdc318d52316c6619
tqspham <tqs.pham@gmail.com>
2026-04-26 13:59:01 +0200

    Create app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home(): React.ReactElement {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <h1 className="text-4xl font-semibold">Hello</h1>
+    </main>
+  );
+}
